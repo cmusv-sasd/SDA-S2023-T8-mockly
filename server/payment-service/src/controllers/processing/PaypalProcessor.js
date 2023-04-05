01f6fdb4e455d475737e85a88b1609c4e633d14f
Xi Huang <xihuang@cmu.edu>
2023-04-05 13:45:52 -0700

    Paypal skeleton

--- server/payment-service/src/controllers/processing/PaypalProcessor.js
+++ server/payment-service/src/controllers/processing/PaypalProcessor.js
@@ -2,9 +2,31 @@ import PaymentProcessor from "./PaymentProcessor"
 
 class PaypalProcessor extends PaymentProcessor {
   async request(payer, payee, amount) {
-    const time = Date.now()
-    // TODO: interact with paypal
-    return { success: true, payer, payee, amount, time }
+    // TODO: get access token of the payer??
+    const accessToken = "REPLACE_WITH_YOUR_ACCESS_TOKEN"
+    return fetch("https://api-m.sandbox.paypal.com/v2/checkout/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        intent: "CAPTURE",
+        purchase_units: [
+          {
+            amount: {
+              value: amount,
+              currency_code: "USD",
+            },
+            payee: {
+              email_address: payee,
+            },
+          },
+        ],
+      }),
+    })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
   }
   
   processResult(result) {
