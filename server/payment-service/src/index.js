1600294f4fd6a8ae914807c38f4ab601e006a947
Xi Huang <xihuang@cmu.edu>
2023-04-06 04:55:54 -0700

    db connection

--- server/payment-service/src/index.js
+++ server/payment-service/src/index.js
@@ -1,15 +1,23 @@
 import express from 'express'
+import cors from 'cors'
+import * as Database from './utils/Database'
 import paymentRoutes from './routes/paymentRoutes'
 
 const PORT = parseInt(process.env.PORT || '3004')
 
-const app = express()
+const app = express().use(
+  cors({
+    origin: ['http://localhost:3001'],
+  })
+)
 
+app.use(express.json())
 app.get('/api/', (req, res) => {
   res.json({ message: 'Hello from Payment' })
 })
 app.use('/payment', paymentRoutes)
 
+Database.connect()
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
